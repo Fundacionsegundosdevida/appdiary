b15a568fb7d4f1c3cde0dfae9ee44d5a75de439b
fundacion <pfundacionsegundosdevida@gmail.com>
2022-10-28 19:39:16 -0500

    Agregando las rutas y metodos get y post

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 // Variables Constantes
 const express = require('express');
 const cors = require('cors');
-
 //agregamos la ruta de la base de datos
 const db = require("./database/db")
+
+const controllers = require('./controllers')// requiriendo la carpeta de controladores
 
 //aplicacion de express
 const app = express();
@@ -11,6 +12,13 @@ const app = express();
 // envio de data desde el fronend al backend desde el mismo puerto
 app.use(cors())
 app.use(express.json()) //permite acceder a la informacion que contiene el body en las peticiones POST
+
+//creacion de las rutas de la APP
+app.get('/user/:id', controllers.getUserByld)// peticion get
+//metodos post "enviar info"
+app.post('/register', controllers.register)
+app.post('/login', controllers.login)
+
 
 
 const PORT = 4000;  //puerto de conexion
