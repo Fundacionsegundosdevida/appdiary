a73f0ef071cc17305cdf131afaa9f0a16190dc90
fundacion <pfundacionsegundosdevida@gmail.com>
2022-10-29 15:39:44 -0500

    Agregando el esquema del formulario de registro

--- controllers/register.js
+++ controllers/register.js
@@ -1,2 +1,13 @@
-const register = async (req, res) => {}
-module.exports = register
+//constantes
+const { Schema, model } = require("mongoose")
+const register = async (req, res) => {} //modulo
+module.exports = register //exportando modulo
+
+//creando un esquema para formulario de nombre correo y contraseña
+const UsuarioShema = new Schema({
+    nombre: {type: String, required: true},
+    correo: {type: String, required: true},
+    contrasena: {type: String, required: true},
+})
+
+module.exports = model("Usuario", UsuarioShema);
