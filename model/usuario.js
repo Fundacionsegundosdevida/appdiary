be91fbc75369324081e5009e255aa6d86a7caf94
fundacion <pfundacionsegundosdevida@gmail.com>
2022-10-29 15:58:44 -0500

    COrrigiendo el esquema en usuario

--- model/usuario.js
+++ model/usuario.js
@@ -1,3 +1,13 @@
 const { model, Schema } = require("mongoose")
 
 //Requerimos el modelo y esquema de mongoose
+
+//creando un esquema para formulario de nombre correo y contraseña
+const UsuarioShema = new Schema({
+    nombre: {type: String, required: true},
+    correo: {type: String, required: true, unique: true},
+    contrasena: {type: String, required: true},
+});
+
+//exportando el modelo y le pasamos el esquema
+module.exports = model("Usuario", UsuarioShema);
