a1472e5c24c5a05d641c8f3bff36030fe3b3a527
fundacion <pfundacionsegundosdevida@gmail.com>
2022-10-29 19:52:33 -0500

    Agregando el back para logueo y verificado con postman

--- controllers/login.js
+++ controllers/login.js
@@ -1,2 +1,28 @@
-const login = async (req, res) => {}
+const bcrypt = require("bcrypt");
+const Usuario = require("../model/Usuario");
+const login = async (req, res) => {
+    const {correo, contraseña} = req.body;
+    Usuario.findOne({correo}).then((usuario)=>{
+        if(!usuario){ //validacion de usuario con correo electronico
+            return res.json({mensaje: "Usuario no encontrado"})
+        }//funcion  para loguear usuario si el correo y contraseña son correctos
+        bcrypt.compare(contraseña, usuario.contraseña).then((esCorrecta) => {
+            if(esCorrecta){ //validando contraseña
+                //obteniendo id y nombre user
+                const {id, nombre,} = usuario
+                //logueo de usuario
+                res.json({
+                    //mensaje para postman
+                    mensaje: "Usuario logueado correctamente",
+                    usuario: {
+                        id,
+                        nombre,
+                }});                
+            }
+            else{ //validacion incorrecta
+                return res.json({mensaje: "Contraseña incorrecta    "})
+            }
+        })
+    });
+}
 module.exports = login
